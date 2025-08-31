85b6def807723352afe7559f581717df80961788
starhaol <jmcgoseng2023@plm.edu.ph>
2025-09-01 00:28:52 +0800

    [UPDATE] tried hashing and input sanitation

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = $_POST["email"];
-        $password = $_POST["password"];
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
         echo "<h1>Form Data Received</h1>";
-
         echo "<strong>Email</strong>: {$email}<br>";
         echo "<strong>Password</strong>: {$password}";
     } else {
--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_POST["name"];
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
         $birthdate = $_POST["birthDate"];
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $password = $_POST["password"];
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
         $consent = $_POST["consent"];
 
         echo "<h1>Form Data Received</h1>";
