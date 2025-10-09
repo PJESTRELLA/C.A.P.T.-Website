10acb6798ccdcd574d2e31edc103592b280729a3
starhaol <jmcgoseng2023@plm.edu.ph>
2025-10-10 00:37:56 +0800

    [BETA] Add Name in User Dashboard - Part 3

--- login/db_access.php
+++ login/db_access.php
@@ -21,16 +21,17 @@
     $email = $_POST['email'] ?? ''; // empty string if email is empty
     $pass  = $_POST['password'] ?? ''; // empty string if password is empty
 
-    $stmt = $mysqli->prepare('SELECT password FROM `user` WHERE email = ? LIMIT 1'); // finds email, limited to one record only
+    $stmt = $mysqli->prepare('SELECT name, password FROM `user` WHERE email = ? LIMIT 1'); // finds email, limited to one record only
     $stmt->bind_param('s', $email); // declares email to find
     $stmt->execute(); // searches the table
     $stmt->store_result(); // generates result (password column)
 
     if ($stmt->num_rows === 1) { // checks if a match has been found
-        $stmt->bind_result($dbpass); // dbpass holds password column
+        $stmt->bind_result($dbname, $dbpass); // dbpass holds password column
         $stmt->fetch(); // returns actual password value
+        
         if ($pass === $dbpass) { // checks if registered password and input password are the same. NOTE: SWAP TO password_verify IF MAGH-HASH
-            $_SESSION['name'] = $name;
+            $_SESSION['name'] = $dbname;
             header('Location: /dashboard/user/user-bulletin.html', true, 303); // true -> replace current header, idk what 303 is T_T
             exit;
         }
