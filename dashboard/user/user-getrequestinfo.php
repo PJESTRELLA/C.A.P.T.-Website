66bc0dbe2ef2c7f9fba41d0a801fc1f4dc93e5d0
starhaol <jmcgoseng2023@plm.edu.ph>
2025-10-10 15:21:24 +0800

    [TEST] Request Input Validation

--- dashboard/user/user-getrequestinfo.php
+++ dashboard/user/user-getrequestinfo.php
@@ -16,6 +16,8 @@
             die("Connection failed: " . $mysqli->connect_error);
         }
 
+        $raiseError = [];
+
         // Part X: Request Option
         $requestoption = $_POST["request_option"] ?? '';
         
@@ -38,75 +40,85 @@
         $telephone = $_POST["telephone"] ?? '';
         $email = $_POST["email"] ?? '';
 
-        $sql = null;
-        $id = null;
-        
-        if (isset($_SESSION['id'])){
-            $id = $_SESSION['id'];
+        if (!preg_match("/^[a-zA-Z ]+$/", $lastname) || !preg_match("/^[a-zA-Z ]+$/", $firstname) || (!empty($middlename) && !preg_match("/^[a-zA-Z ]+$/", $middlename))) {
+            $raiseError[] = "[ERROR]: Names can only contain letters and spaces.";
+        } 
+
+        if (!preg_match("/^[0-9A-Za-z ]+$/", $house)) {
+            $raiseError[] = "[ERROR]: House number can only contain letters, numbers, and spaces.";
         }
 
-        switch($requestoption){
-            case 'clearance':
-                $sql = "INSERT INTO `requestedDocument_BarangayClearance` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                break;
-            case 'residency':
-                $sql = "INSERT INTO `requestedDocument_BarangayCertificateResidency` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                break;
-            case 'indigency':
-                $sql = "INSERT INTO `requestedDocument_BarangayCertificateIndigency` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                break;
-            case 'business':
-                $sql = "INSERT INTO `requestedDocument_BarangayCertificateBusiness` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                break;
-            default:
-                die("[ERROR]: Unknown request option.");
+
+        if (!preg_match("/^[a-zA-Z0-9. ]+$/", $street)){  // lower, upper, space, numbers, period
+            $raiseError[] = "[ERROR]: Street name can only contain letters, numbers, and periods"; 
         }
 
-        try {
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("isssssisssssssss", $id, $lastname, $firstname, $middlename, $suffix, $birthday, $age, $gender, $house, $street, $barangay, $city, $zip, $phone, $telephone, $email);
-            $stmt->execute(); // insert actual values into the table 
+        if (!preg_match("/^[a-zA-Z ]+$/", $city)){  // lower, upper, space
+            $raiseError[] = "[ERROR]: City names can only contain letters and spaces."; 
+        }
 
-            if ($stmt->affected_rows === 1) { // checks if one row has been added to the table
-                header("Location: /dashboard/user/user-request.html"); // redirect to request page
-                exit(); // stops code execution
-            } else {
-                echo "[WARNING]: Insert ran but no row added.";
+        if (!preg_match("/^[0-9]{4}$/", $zip)) {
+            $raiseError[] = "[ERROR]: ZIP Code must contain exactly 4 digits.";
+        }
+
+        if (!preg_match("/^09[0-9]{9}$/", $phone)) {
+            $raiseError[] = '[ERROR]: Phone number must start with "09" and contain exactly 11 digits.';
+        }
+
+        if (!empty($telephone) && !preg_match("/^8[0-9]{7}$/", $telephone)) {
+            $raiseError[] = '[ERROR]: Telephone number must start with "8" and contain exactly 8 digits.';
+        }
+
+        if (empty($raiseError)){
+            $sql = null;
+            $id = null;
+            
+            if (isset($_SESSION['id'])){
+                $id = $_SESSION['id'];
             }
-                $stmt->close(); // releases stmt (since dynamically allocated)
-        } catch (mysqli_sql_exception $e) {
-            echo "[ERROR]: " . $e->getMessage();
+
+            switch($requestoption){
+                case 'clearance':
+                    $sql = "INSERT INTO `requestedDocument_BarangayClearance` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    break;
+                case 'residency':
+                    $sql = "INSERT INTO `requestedDocument_BarangayCertificateResidency` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    break;
+                case 'indigency':
+                    $sql = "INSERT INTO `requestedDocument_BarangayCertificateIndigency` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    break;
+                case 'business':
+                    $sql = "INSERT INTO `requestedDocument_BarangayCertificateBusiness` (`userId`, `lastName`, `givenName`, `middleName`, `suffix`, `birthday`, `age`, `gender`, `houseNumber`, `street`, `barangay`, `city`, `zip`, `phoneNumber`, `telephoneNumber`, `emailAddress`)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    break;
+                default:
+                    die("[ERROR]: Unknown request option.");
+            }
+
+            try {
+                $stmt = $mysqli->prepare($sql);
+                $stmt->bind_param("isssssisssssssss", $id, $lastname, $firstname, $middlename, $suffix, $birthday, $age, $gender, $house, $street, $barangay, $city, $zip, $phone, $telephone, $email);
+                $stmt->execute(); // insert actual values into the table 
+
+                if ($stmt->affected_rows === 1) { // checks if one row has been added to the table
+                    header("Location: /dashboard/user/user-request.html"); // redirect to request page
+                    exit(); // stops code execution
+                } else {
+                    echo "[WARNING]: Insert ran but no row added.";
+                }
+                    $stmt->close(); // releases stmt (since dynamically allocated)
+            } catch (mysqli_sql_exception $e) {
+                echo "[ERROR]: " . $e->getMessage();
+            }
+        } else{
+            foreach ($raiseError as $error) {
+                echo htmlspecialchars($error) . "<br>";
+            }
         }
-
-        /*echo "<h1>Form Data Received</h1>";
-
-        echo "<h2>Document Request</h2>";
-        echo "<strong>Request Option</strong>: {$requestoption}<br><br>";
-
-        echo "<h2>Part 1: Personal Information</h2>";
         
-        echo "<strong>Last Name</strong>: {$lastname}<br>";
-        echo "<strong>First Name</strong>: {$firstname}<br>";
-        echo "<strong>Middle Name</strong>: {$middlename}<br>";
-        echo "<strong>Suffix</strong>: {$suffix}<br>";
-        echo "<strong>Birthday</strong>: {$birthday}<br>";
-        echo "<strong>Age</strong>: {$age}<br>";
-        echo "<strong>Gender</strong>: {$gender}<br><br>";
-
-        echo "<h2>Part 2: Contact Information</h2>";
-        echo "<strong>House</strong>: {$house}<br>";
-        echo "<strong>Street</strong>: {$street}<br>";
-        echo "<strong>Barangay</strong>: {$barangay}<br>";
-        echo "<strong>City</strong>: {$city}<br>";
-        echo "<strong>ZIP Code</strong>: {$zip}<br>";
-        echo "<strong>Phone No.</strong>: {$phone}<br>";
-        echo "<strong>Telephone No.</strong>: {$telephone}<br>";
-        echo "<strong>Email</strong>: {$email}<br>";*/
-
     } else {
         echo "<h1>No data submitted!</h1>";
     }
